5d8abcaeb856ffc41a0d7bfeda457626afdeb726
jeanrabello <jean.rabello.prog@gmailcom>
2023-07-02 16:43:31 -0300

    Add section 10 -> L126 to L131

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="resource in resources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+  props: ['resources'],
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  margin: auto;
+  padding: 0;
+  max-width: 40rem;
+}
+</style>
